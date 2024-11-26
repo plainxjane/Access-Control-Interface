4e2b24ecf3ff9ec9f00b90fb3b4c74b23fc10fdb
Jane <janee1809xx@gmail.com>
2024-11-26 17:44:53 +0800

    Add Layer form

--- main.py
+++ main.py
@@ -1,32 +1,57 @@
 from flask import Flask, flash, request, redirect, render_template, url_for
-from models.Forms import LoginForm
+from models.Forms import LoginForm, AddLayerForm, AddUserForm, UpdateUserForm
 
 app = Flask(__name__)
 
 # config app
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('homepage.html')
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     login_form = LoginForm()
     if request.method == 'POST' and login_form.validate_on_submit():
         password = login_form.password.data
-        print('Password:', password)
         if password == 'S&Lrail8':
             flash('Login Successful!')
             return redirect(url_for('database'))
         else:
             flash('Login Unsuccessful. Please try again.')
 
-    return render_template('login.html', login_form=login_form)
+    return render_template('login.html', form=login_form)
+
+
+@app.route('/add_layer', methods=['POST', 'GET'])
+def add_layer():
+    add_layer_form = AddLayerForm()
+    if request.method == 'POST' and add_layer_form.validate_on_submit():
+        name = add_layer_form.name.data
+        group = add_layer_form.group.data
+
+        print("Layer added successfully!")
+
+    return render_template('add_layer.html', form=add_layer_form)
+
+
+@app.route('/add_user', methods=['POST', 'GET'])
+def add_user():
+    pass
+
+
+@app.route('/update_user', methods=['POST', 'GET'])
+def update_user():
+    pass
+
 
 @app.route('/database')
 def database():
     return render_template('database.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models/Forms.py
+++ models/Forms.py
@@ -1,8 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, Optional,ValidationError
+from wtforms import StringField, TextAreaField, SelectField, SelectMultipleField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Optional, ValidationError
 from wtforms import validators
+from wtforms.widgets import ListWidget, CheckboxInput
+
 
 class LoginForm(FlaskForm):
     password = PasswordField('Password')
     submit = SubmitField('Submit')
+
+
+class AddLayerForm(FlaskForm):
+    name = StringField('Name of layer')
+    group = SelectMultipleField('Select group to add layer to', choices=[('IDE General Viewers', 'IDE General Viewers'),
+                                                                         ('Editors_ARCH', 'Editors_ARCH'),
+                                                                         ('Editors_CRI', 'Editors_CRI'),
+                                                                         ('Editors_CSV', 'Editors_CSV'),
+                                                                         ('Editors_CTIPS', 'Editors_CTIPS'),
+                                                                         ('Editors_GTT', 'Editors_GTT'),
+                                                                         ('Editors_LAND', 'Editors_LAND')],
+                                widget=ListWidget(prefix_label=False),
+                                option_widget=CheckboxInput(),
+                                )
+    submit = SubmitField('Submit')
+
+
+class AddUserForm(FlaskForm):
+    pass
+
+
+class UpdateUserForm(FlaskForm):
+    pass
